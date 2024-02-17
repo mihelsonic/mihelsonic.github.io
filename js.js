724b438c2c2b804a2f2af3415afd6cbdb683e528
mihelsonic <160264774+mihelsonic@users.noreply.github.com>
2024-02-17 10:42:41 +0300

    js.js

--- js.js
+++ js.js
@@ -1,10 +1,25 @@
+document.getElementById('myButton').addEventListener('click', function() {
+    // Play the audio
+    var myAudio = document.getElementById('myAudio');
+    myAudio.play();
+
+    // Change the background image
+    var body = document.body;
+    body.classList.add('changed-background');
+
+    // Change the background back to color after 15 seconds
+    setTimeout(function() {
+        body.classList.remove('changed-background');
+    }, 10000);
+});
+
 /* Это объявление переменной, мы наши кнопку по тегу */
 const button = document.querySelector('button');
 
 /* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
-button.addEventListener('click', function() {
+/*button.addEventListener('click', function() {
 	alert('Если волк молчит, то лучше его не перебивать.')
-})
+})*/
 const btnUp = {
 	el: document.querySelector('.btn-up'),
 	show() {
